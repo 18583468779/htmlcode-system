0db31b38db38034269758ec937eefdcc8d3e1c88
xiewen <1123654054@qq.com>
2024-09-13 21:34:58 +0800

    修改帖子表

--- database/migrations/2024_09_13_130111_create_tipics_table.php
+++ database/migrations/2024_09_13_130111_create_tipics_table.php
@@ -11,8 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('tipics', function (Blueprint $table) {
+        Schema::create('topics', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained('users')->onDelete('cascade'); // 关联，当删除当前用户，那么当前用户的帖子一并删除
+            $table->string('title');
+            $table->text('content');
+            $table->text('filename')->nullable()->comment('文件名，在项目根目录markdown目录');
+            $table->unsignedInteger('favour_count')->default(0)->comment('点赞数');
+            $table->unsignedInteger('favorite_count')->default(0)->comment('收藏数');
+            $table->boolean('recommend')->default(false)->comment('推荐');
             $table->timestamps();
         });
     }
@@ -22,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('tipics');
+        Schema::dropIfExists('topics');
     }
 };
