d84efc4d704381478fd9c3e34f8a5ffc87615ab1
xiewen <1123654054@qq.com>
2024-09-13 21:24:40 +0800

    新增帖子模型（tipic）

---
+++ app/Http/Controllers/TipicController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tipic;
+use App\Http\Requests\StoreTipicRequest;
+use App\Http\Requests\UpdateTipicRequest;
+
+class TipicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTipicRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Tipic $tipic)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Tipic $tipic)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTipicRequest $request, Tipic $tipic)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Tipic $tipic)
+    {
+        //
+    }
+}
