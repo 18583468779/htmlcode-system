79dd44e8b981cebafc7a99c382de8136c89b71d9
xiewen <1123654054@qq.com>
2024-09-19 20:00:30 +0800

    创建资源路由

--- app/Http/Controllers/TipicController.php
+++ app/Http/Controllers/TipicController.php
@@ -14,6 +14,7 @@ class TipicController extends Controller
     public function index()
     {
         //
+        return Tipic::all(); //返回所有的数据
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\TipicController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+// 创建资源路由，排除create,edit方法
+Route::resource('tipic', TipicController::class)->except(['create', 'edit']);
