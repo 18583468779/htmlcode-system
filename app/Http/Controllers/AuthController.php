9e79f49b2e5361a91fed8e09855c5b4c24abd72d
xiewen <1123654054@qq.com>
2024-09-21 21:05:36 +0800

    登录验证用户不存在

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,14 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
+    private function getUser(string $name)
+    {
+        // 获取当前用户
+        $user = User::orWhere('name', $name);
+        foreach (['email', 'mobile'] as $field) {
+            $user->orWhere($field, $name);
+        }
+        return $user->first();
+    }
     public function login(Request $request)
     {
-        return $request->input();
+        $user = $this->getUser($request->input('name'));
+        $request->validate([
+            'name' => ['required', function ($attr, $value, $fail) use ($user) {
+                if (!$user) {
+                    $fail('用户不存在，请检查账号');
+                }
+            }],
+            'password' => 'required'
+        ]);
+        return $user;
     }
     public function register() {}
 }
